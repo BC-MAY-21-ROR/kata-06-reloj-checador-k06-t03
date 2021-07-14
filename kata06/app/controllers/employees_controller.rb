e5cf73a45f7d234c2196a9c4e29245262742ec0b
AlejandroVI99 <avi200599@gmail.com>
2021-07-14 12:50:56 -0500

    Edit view created
    We created the view Edit to edit an employee and we added some link_to
    in the index view, one for redirect to the edit view and another for delete an employee.
    Also we created some methods in the employee controller to do the actions edit and delete.
    
    Co-authored-by: SaraNavarro  <esaranm@gmail.com>
    Co-authored-by: martinmt-mx <mmojica0@ucol.mx>
    Co-authored-by: OmarRoBra <jrodriguez40@ucol.mx>

--- kata06/app/controllers/employees_controller.rb
+++ kata06/app/controllers/employees_controller.rb
@@ -12,4 +12,24 @@ class EmployeesController < ApplicationController
 
         redirect_to employees_path
     end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        data = params.require(:employee).permit(:name, :email, :posicion, :num_empleado, :private_num)
+        @employee.update(data)
+
+        redirect_to employees_path
+    end
+
+    def destroy
+        @employee = Employee.find(params[:id])
+        @employee.destroy
+
+        redirect_to employees_path
+    end
+
 end
