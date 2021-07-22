ada139787858357e2642a5cde84fb4c75491c6da
AlejandroVI99 <avi200599@gmail.com>
2021-07-22 14:59:53 -0500

    Modified files
    In this files were added some gems, some routes and were added a button in the
    main layout.

--- kata06/db/schema.rb
+++ kata06/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_184135) do
+ActiveRecord::Schema.define(version: 2021_07_22_162901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admins", force: :cascade do |t|
+  create_table "administrators", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
