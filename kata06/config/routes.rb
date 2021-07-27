288aa3241a1612d7210e9f730ed14be06971a252
OmarRoBra <50344283+OmarRoBra@users.noreply.github.com>
2021-07-26 20:01:03 -0500

    !

--- kata06/config/routes.rb
+++ kata06/config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :employees
   get 'loginE', to: 'employees#login'
+  post 'loginE', to: 'employees#checkIn'
+  get 'logoutE', to: 'employees#login'
+  post 'logoutE', to: 'employees#logout'
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
   post 'login', to: 'sessions#create'
+
 
   resources :reports
   resources :companies
--- kata06/db/migrate/20210722081528_rename_column_and_connect_admin_to_reports.rb
+++ kata06/db/migrate/20210722081528_rename_column_and_connect_admin_to_reports.rb
@@ -1,7 +1,5 @@
 class RenameColumnAndConnectAdminToReports < ActiveRecord::Migration[6.1]
   def change
-    add_reference :reports, :admin, index: true
-    add_foreign_key :reports, :admins
     remove_column :reports, :type, :string
     add_column :reports, :report_type, :string
   end
